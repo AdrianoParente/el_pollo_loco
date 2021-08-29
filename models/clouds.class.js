8dd6c7952313662febc2415e94cd46a7c82bc4d0
Adriano Parente <adriano.parente@gmx.de>
2021-08-29 21:21:39 +0200

    object animation

--- models/clouds.class.js
+++ models/clouds.class.js
@@ -1,11 +1,18 @@
 class Clouds extends MoveableObject {
+
     y = 20;
     width = 500;
     height = 250;
+   
     constructor() {
         super().loadImage('img/5.Fondo/Capas/4.nubes/1.png')
 
         this.x = Math.random() * 500;
-
+        this.animate();
     }
+    animate() {
+      this.moveLeft();
+    }
+
+   
 }
